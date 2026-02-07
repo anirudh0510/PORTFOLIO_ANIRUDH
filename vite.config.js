cc26cc064ccc70c3812255257f204e65e4ce31e6
Anirudh Chauhan <anirudh102005@gmail.com>
2026-02-07 09:37:39 +0530

    deploy portfolio to vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import react from "@vitejs/plugin-react";
 export default defineConfig({
-  base: "/PORTFOLIO_ANIRUDH/",
   plugins: [react(), tailwindcss()],
 });
